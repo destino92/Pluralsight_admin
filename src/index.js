ce6ab3a752acbb324f4aff141ac73d555fe136b5
destino92 <destinodello@gmail.com>
2017-01-23 14:18:14 +0300

    feat: instantiate Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
+import {Provider} from 'react-router';
 import routes from './routes';
 import './styles/styles.css'; //Webpack
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  // Provider allows us to have access to the store
+  // in every component
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,6 +6,7 @@ export default function configureStore(initialState){
   return createStore(
     rootReducer,
     initialState,
+    // Read more about this
     applyMiddleware(reduxImmutableStateInvariant())
   );
 }
