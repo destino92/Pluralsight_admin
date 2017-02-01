8be8a596aeb7ce205defec63991b4f2120e76cd4
destino92 <destinodello@gmail.com>
2017-02-01 19:28:48 +0300

    feat: Created author reducer

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { Router, browserHistory } from 'react-router';
 import {Provider} from 'react-redux';
 import routes from './routes';
 import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css'; //Webpack
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   // Provider allows us to have access to the store
